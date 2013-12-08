8c78c9ac2e591c64b543904c1c3687bce5423eb5
Nick Escallon <nickescallon@gmail.com>
2013-12-08 17:46:12 -0500

    Fixed Parse for multiple values. - Nested objects only work if they are the last value in the main obj - Arrays still do not work

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,185 +3,72 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-	
-	//handle numbers, bools, and nulls
-	if (typeof json === 'number' || typeof json === 'boolean' || json == null) { 
-	 	return json;
-	}
 
-	//handle strings of bools and nulls
-	if (json === "true") {return true};
-	if (json === "false") {return false};
-	if (json === "null") {return null};
+	var walk = function(str, remain) {
 
-	//handle strings
-	if(!(/[\{\}\[\],]/.test(json))){
-		return json;
-	}
-
-	//sets result based on json obj
-	if (json[0] == '{') {var result = {}, obj = true};
-	//if (json[0] == '[') {var result = [], arr = true};
-
-	//handle object values
-	if (json[0] == ':') {
-		console.log(json[1] + " is string val [1]");
-		if (json[1] == '{') {
-			var remainder = json.substr(1, json.length-1);
-			var value = parseJSON(remainder);
+		//handle numbers, bools, and nulls
+		if (typeof str === 'number' || typeof str === 'boolean' || str == null) { 
+	 		return str;
 		}
 
-		if (json[1] === '"') {
-			var value = '';
-			var count = 1;
-			while (json[count] != ',' && json[count] != '}') {
-				if (json[count] != '"'){
-					value += json[count];
-					//console.log(value);
-				}
-				count++;
+		//handle strings of bools and nulls
+		if (str === "true") {return true};
+		if (str === "false") {return false};
+		if (str === "null") {return null};
+
+		//handle strings
+		if(!(/[\{\}\[\],]/.test(str))){
+			return str;
+		}
+
+		//walk through str
+		var strArr = [];
+		for (var i=0; i<str.length; i++){
+			if (!(/[\{\}\[\],\": ]/.test(str[i]))) {
+					strArr.push(str[i]);
 			}
-		}
-
-		return value;
-	}
-
-	//handle commas for objects
-	if (json[0] == ',') {
-		parseJSON(walk(json));
-	}
-
-	//handle objects
-	if (obj){
-		parseJSON(walk(json));
-	}
-
-	var walk = function(str, remainder) {
-		var key = [];
-		if (remainder == undefined) {
-			for (var i=1; i<str.length; i++) {
-				if (!(/[\{\}\[\],\": ]/.test(str[i]))) {
-					key.push(str[i]);
-				}
-
-				if (str[i] == ':') {
-					var keyStr = key.join('');
+			if (str[i] == ':' || str[i] == '}' || str[i] == ',') {
+				if (i != 0){
+					var val = strArr.join('');
 					var remainder = str.substr(i, str.length-(i));
-					var val = parseJSON(keyStr);
-					result[keyStr] = val;
-					return remainder;
+					break;
 				}
 			}
 		}
+
+		//
+		if (str[0] == ':' || str[0] == ','){
+			if (str[1] == '{'){
+				var remainder = str.substr(1, str.length-(1));
+				val = walk(remainder);
+			}
+			return val;
+		}
+
+		//
+		if (str[0] == '}') {
+			return str;
+		}
+
+
+		if (str[0] == '{'){
+			var result = {};
+			while(remainder.slice(-1) == '}' && (/[a-zA-Z]/).test(remainder) && remainder.length > 2){
+				var value = walk(remainder);
+				result[val] = value;
+				var newRemainder = remainder.substr(value.length+3, remainder.length - value.length+3);
+				remainder = newRemainder;
+				val = walk(remainder);
+				newRemainder = remainder.substr(val.length+3, remainder.length - val.length+3);
+				remainder = newRemainder;
+			}
+
+		}
+		return result;
   	};
 
-  
-	return result;
+  	return walk(json);
+
 };
 
-
-
-//json '{"id":"value"}'
-		//'"id:"value"'
-
-
-
-
-
-/*if (obj){
-		//remove spaces
-		json.split(' ').join('');
-		var newStr = json.substr(2, json.length-3);
-		var key = '';
-		var count = 0;
-		if (newStr.indexOf(':') != -1){
-			while (newStr[count+1] != ':' && newStr[count+1] != ','){
-				key += newStr[count];
-				count++;
-			}
-		}
-		var remainder = newStr.substr(count+2, newStr.length-4);
-		if (remainder[0] == '{' || remainder[0] == '['){
-			result[key] = parseJSON(remainder);
-		}
-
-	}
-*/
-
-
-/*//creates an array where each index is a key value pair, removes all spaces
-  	var strArr = json.split(' ').join('').split(',');
-
-  	//iterates through said arrray
-  	for (var i=0; i<strArr.length; i++){
-
-
-  		//creates an array splitting keys and values
-  		var keyVal = strArr[i].split(':');
-  	
-  		var key = (Object.keys(result).length) ? keyVal[0].slice(1, keyVal[0].length-1) : keyVal[0].slice(2, keyVal[0].length-1);
-
-  	
-  		//Handle objects and arrays (currently not arrays)
-  			if (keyVal[1][0] == '['){
-  				if(keyVal[2]){
-  					while (keyVal[2].indexOf(']') == -1){
-  						keyVal[1]+= ',' + keyVal[2];
-  						keyVal.splice(2, 1);
-  					}
-  				}
-  				keyVal[1]+= ',' + keyVal[2] + ']';
-  				keyVal.splice(2,1);
-  				keyVal[1] = keyVal[1].replace(']]', '');
-  				//keyVal[1] = keyVal[1].substr(0, keyVal[1].length-1);
-  				var val = parseJSON(keyVal[1]);
-  			}
-  	
-  			if (keyVal[1][0] == '{'){;
-  				if(keyVal[2]){
-  					while (keyVal[2].indexOf('}') == -1){
-  						keyVal[1]+= ':' + keyVal[2];
-  						keyVal.splice(2, 1);
-  					}
-  				}
-  				keyVal[1]+= ':' + keyVal[2] + '}';
-  				keyVal.splice(2,1);
-  				keyVal[1] = keyVal[1].replace('}}', '');
-  				//keyVal[1] = keyVal[1].substr(0, keyVal[1].length-1);
-  				var val = parseJSON(keyVal[1]);
-  			}else {
-  				var val = (keyVal[1].substr(-1) == '}') ? keyVal[1].slice(1, keyVal[1].length-1) : keyVal[1].slice(1, keyVal[1].length-1);
-  			}
-
-  			result[key] = val;
-  	
-	}*/
-
-
-
-
-/*
-
-str = '{"id":"file","value":"File","popup":"test"}'
-//"{"id":"file","value":"File","popup":"test"}"
-array = str.split(',');
-//["{"id":"file"", ""value":"File"", ""popup":"test"}"]
-nested = array[0].split(':');
-//["{"id"", ""file""]
-newobj={};
-//Object {}
-newobj[nested[0][2]+nested[0][3]] = nested[1][0]+nested[1][1]+nested[1][2]+nested[1][3]+nested[1][4]+nested[1][5];
-//Object {id: ""file""}
-
-teststr2 = "{"id":"file","value":"File","popup":"test","obj":"{"key":"val"}}""
-
-//not working
-
-teststr3 = '{"id":"file", "id2": {"file2":"val"}}'
-//"{"id":"file", "id2": {"file2":"val"}}"
-var strArr = teststr3.split(' ').join('').split(',');
-//["{"id":"file"", ""id2":{"file2":"val"}}"]
-var nested = strArr[1].split(':');
-//[""id2"", "{"file2"", ""val"}}"] //this splits up the objects. need to join them somehow
-
-*/
+	
