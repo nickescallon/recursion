300937d2b3d762f2df599136fc001f20544f7186
Hack Reactor Students <nickescallon@gmail.com>
2014-01-06 22:04:06 -0800

    Some progress on Parse

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,8 +3,73 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-	
+  var currentChar = 0;
+  
+  var walk = function(){
+    //Case for :
+    if (json[currentChar] === ':' || json[currentChar] === ','){
+      currentChar++;
+    }
 
+    //Case for whitespace
+    if (json[currentChar] === ' '){
+      currentChar++;
+      while (json[currentChar] === ' '){
+        currentChar++;
+      }
+      return walk(json[currentChar]);
+    }
+
+    //Case for numbers
+    if (parseInt(json[currentChar])){
+      var result = '';
+      result += json[currentChar];
+      currentChar++;
+      while (parseInt(json[currentChar])){
+        result += json[currentChar];
+        currentChar++;
+      }
+
+      return parseInt(result);
+    }
+
+    //Case for strings
+    if (json[currentChar] === '"'){
+      var result = '';
+      currentChar++;
+      while (json[currentChar] !== '"' ){
+        result += json[currentChar];
+        currentChar++;
+      }
+      currentChar++;
+      return result;
+    }
+    //Case for objects]
+    if (json[currentChar] === '{'){
+      var result = {};
+      currentChar++;
+      while (json[currentChar] !== '}' && json[currentChar]){
+        var key = walk(json[currentChar]);
+        result[key] = walk(json[currentChar]);
+        currentChar++;
+      }
+      return result;
+    }
+
+    //Case for arrays
+    if (json[currentChar] === '['){
+      var result = [];
+      currentChar++;
+      while (json[currentChar] !== ']' && json[currentChar]){
+        result.push(walk(json[currentChar]));
+        currentChar++;
+      }
+      return result;
+    }
+  }
+
+
+  return walk(json);
 };
 
-	
+
